3920b8ddafd5d7e476d929ab7e36249c5005056c
claudiacole96 <claudia.evans@bryanuniversity.edu>
2020-10-28 19:45:05 -0400

    week2

---
+++ week2/basic_website_wJS/script.js
@@ -0,0 +1,36 @@
+var nav = document.createElement("nav");
+document.body.appendChild(nav);
+var navLink = document.createElement("a");
+var navLink2 = document.createElement("a");
+var navLink3 = document.createElement("a");
+navLink.href = "./index.html";
+navLink.textContent = "Home";
+navLink2.textContent = "About";
+navLink3.textContent = "Contact";
+nav.appendChild(navLink);
+nav.appendChild(navLink2);
+nav.appendChild(navLink3);
+
+var main = document.createElement("main");
+document.body.appendChild(main);
+var h1 = document.createElement("h1");
+h1.textContent = "This is my first JS website";
+main.appendChild(h1);
+var p = document.createElement("p");
+p.textContent = "It wasn't as bad as I expected!!!";
+main.appendChild(p);
+var ul = document.createElement("ul");
+main.appendChild(ul);
+var li = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+li.textContent = "First the HTML";
+li2.textContent = "Then the Javascript";
+li3.textContent = "Finally put it all together!!";
+ul.appendChild(li);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+var footer = document.createElement("footer");
+footer.textContent = "2020 Web Dev Student"
+document.body.appendChild(footer);
