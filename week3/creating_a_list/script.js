e9217e14512e5ed22060b87bcb7ca7212e4db4b7
claudiacole96 <claudia.evans@bryanuniversity.edu>
2020-11-06 18:05:53 -0500

    week3

---
+++ week3/creating_a_list/script.js
@@ -0,0 +1,28 @@
+//1 Link JS to HTML
+
+//2
+var h1 = document.createElement("h1");
+h1.textContent = "Hello World";
+document.body.appendChild(h1);
+
+//3
+for ( i = 0; i < 10; i++ ) {
+    var newH1 = document.createElement("h1");
+    newH1.textContent = "Hello World";
+    newH1.classList.add("header");
+    document.body.appendChild(newH1);
+}
+
+//4
+var headerClass = document.getElementsByClassName("header");
+for ( i = 0; i < headerClass.length; i++ ) {
+    headerClass[i].style.color = "blue";
+}
+
+//5
+var names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+
+//6
+for ( i = 0; i < names.length; i++ ) {
+    console.log(names[i]);
+}
